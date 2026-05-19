875fe9aaf286c81932ce273031b6a7e3cf4a066f
Satyam-R4j <satyamraj418@gmail.com>
2026-05-19 11:15:31 +0530

    working on sensor.js

--- car.js
+++ car.js
@@ -11,15 +11,56 @@ class Car {
         this.friction = 0.05
         this.angle = 0
 
-        
+        this.damaged = false
+
+        this.sensor = new Sensor(this)
         this.controls = new Controls()
     }
 
-    update() {
+    update(roadBorders) {
         this.#move()
+        
+        this.polygon = this.#createPolygon()
+        this.damaged = this.#assessDamage(roadBorders)
+        this.sensor.update(roadBorders)
     }
 
-    #move(){
+    #assessDamage(roadBorders)
+    {
+        for(let i = 0; i < roadBorders.length; i++){
+            if(polysIntersect(this.polygon, roadBorders[i]))
+            {
+                return true
+            }
+
+        }
+        return false
+    }
+
+    #createPolygon() {
+        const points = []
+        const rad = Math.hypot(this.width, this.height) / 2
+        const alpha = Math.atan2(this.width, this.height)
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad,
+        })
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad,
+        })
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad,
+        })
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad,
+        })
+        return points
+    }
+
+    #move() {
         if (this.controls.forward) {
             this.speed += this.acceleration
         }
@@ -40,17 +81,16 @@ class Car {
             this.speed += this.friction
         }
 
-        if(Math.abs(this.speed) < this.friction)
-        {
+        if (Math.abs(this.speed) < this.friction) {
             this.speed = 0
         }
 
-        if(this.speed != 0){
+        if (this.speed != 0) {
 
 
             const flip = this.speed > 0 ? 1 : -1;
             if (this.controls.left) {
-                this.angle += 0.03  * flip
+                this.angle += 0.03 * flip
             }
             if (this.controls.right) {
                 this.angle -= 0.03 * flip
@@ -62,17 +102,17 @@ class Car {
     }
 
     draw(ctx) {
-        ctx.save()
-        ctx.translate(this.x, this.y)
-        ctx.rotate(-this.angle)
+
         ctx.beginPath()
-        ctx.rect(
-            - this.width / 2,
-            - this.height / 2,
-            this.width,
-            this.height
-        )
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y)
+        for (let i = 1; i < this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y)
+        }
         ctx.fill()
-        ctx.restore()
+        this.sensor.draw(ctx)
     }
+}
+
+function polysIntersect(poly1, poly2){
+    
 }
